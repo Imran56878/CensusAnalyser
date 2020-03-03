65b45d0033931267de860f13e9c363ba6ed4e225
Imran56878 <mi056878@gmail.com>
2020-03-03 16:07:51 +0530

    [imran]Add.WelcomeMessage

---
+++ CensusAnalyserTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Census Analyser");
+        }
+    }
+}
