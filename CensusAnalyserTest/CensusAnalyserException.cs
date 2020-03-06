56f2bf627cfe24e3a5b47e13c8c65ff507be7f1d
Imran56878 <mi056878@gmail.com>
2020-03-06 10:59:36 +0530

    [Imran]Added TestCase1StateCsvLoadData

---
+++ CensusAnalyserTest/CensusAnalyserException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyserTest
+{
+    /// <summary>
+    /// CustomExceptionClass and inherit
+    /// Exception class  
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+  public  class CensusAnalyserException :Exception
+    {
+        string message;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CensusAnalyserException"/> class.
+        /// </summary>
+        /// <param name="msg">The MSG.</param>
+        public CensusAnalyserException(string msg)
+        {
+            this.message = msg;
+        }
+        /// <summary>
+        /// Gets the ge message.
+        /// </summary>
+        /// <value>
+        /// The ge message.
+        /// </value>
+        public string GetMessage { get => this.message ; }
+    }
+}
---
+++ CensusAnalyserTest/StateCensusAnalyser.cs
@@ -0,0 +1,53 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using Microsoft.VisualBasic.FileIO;
+
+namespace CensusAnalyserTest
+{
+    public class StateCensusAnalyser
+    {
+        int totalRecord = 0;
+        public int LoadStateData(string path, char delimiter = ',')
+        {
+            try
+            {
+                if (Path.GetExtension(path) != ".csv")
+                {
+                    throw new CensusAnalyserException("Wrong_File_Extension");
+                }
+
+                else if (!File.Exists(path))
+                {
+                    throw new CensusAnalyserException("File_Not_Exist");
+                }
+                string[] str = File.ReadAllLines(path);
+                foreach (string line in str)
+                {
+                  //  totalRecord++;
+                    string[] csvline = line.Split(delimiter);
+                    if(csvline.Length !=4 && csvline.Length != 2)
+                    {
+                        throw new CensusAnalyserException("Wrong_Delimiter");
+                    }
+
+                }
+                if (str[0] != "State,Population,AreaInSqKm,DensityPerSqKm")
+                {
+                    throw new CensusAnalyserException("Wrong paramter ");
+                }
+                totalRecord = str.Length;
+            return totalRecord;
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw ;
+            }
+        }
+    }
+}
